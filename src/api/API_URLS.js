507e683c9d0de840631586a169f839d76cc4a1db
Jimmy Guo <jimmyzidanguo@hotmail.com>
2018-07-19 04:22:48 -0400

    asd

--- src/api/API_URLS.js
+++ src/api/API_URLS.js
@@ -2,5 +2,9 @@
 // toggle these to access development versus production servers
 
 // DEV
-export const ADMIN_MICROSERVICE = 'https://localhost:7301'
-export const AWS_FEDERATED_IDENTITY_ENV = 'us-east-1:c23df06a-8a0f-454d-bf66-5d1a231a8f1e'
+// export const ADMIN_MICROSERVICE = 'https://localhost:7301'
+// export const AWS_FEDERATED_IDENTITY_ENV = 'us-east-1:c23df06a-8a0f-454d-bf66-5d1a231a8f1e'
+
+// PROD
+export const ADMIN_MICROSERVICE = 'https://rentheros.com:7301'
+export const AWS_FEDERATED_IDENTITY_ENV = 'us-east-1:55fcf453-857b-41ac-8cb7-9a64306fa50d'
